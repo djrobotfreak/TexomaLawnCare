5590c4d7620d6ad11093f5989aa6e396d39110e9
Derek Wene <djrobotfreak@gmail.com>
2014-05-11 00:46:14 -0500

    added things to know info

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+        <div class="footer">
+            <p>&copy; Texoma Lawn Care 2014</p>
+        </div>        
+
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
