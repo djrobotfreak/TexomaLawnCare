6d18d559e5bd28ea4407b571864cf3fd55eb14b2
Derek Wene <djrobotfreak@gmail.com>
2014-05-11 00:14:03 -0500

    finished front page with new nav and test things to know page

--- header.php
+++ header.php
@@ -40,16 +40,30 @@
         </ul>
         <h3 class="text-muted">Project name</h3>
       </div>
--->
-
+--> <div class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            </ul>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Texoma Lawn Care</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Things to know</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
         
-        <div class="jumbotron">
-            <div class="thumbnail" style = 'width:20%; float:left;'>
-                <img src="png/icon.png" alt="...">
-            </div>
-            <div class="thumbnail" style = 'width:19%; float:right;'>
-                <img src="png/icon.png" alt="...">
-            </div>            
+        <div class="jumbotron">        
+                <img src="png/icon.png" alt="..." style = 'width:20%; float:left;'>        
+                <img src="png/icon.png" alt="..." style = 'width:20%; float:right;'>       
             <div style="width:60% position:center">
                 <h1>Texoma Lawn Care</h1>
                 <p class="lead">903-640-3404</p>
@@ -57,6 +71,10 @@
             </div>
 <!--        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>-->
         </div>
+           
+            
+
+            
     </body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
---
+++ php/things.php
@@ -0,0 +1,5 @@
+<?php include("header.php");?> 
+<?php include("things_to_know.html");?> 
+<?php include("footer.php");?>
+
+hi
